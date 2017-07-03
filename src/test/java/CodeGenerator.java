d7c76c91ce677488476c29aeb42a3d564b94731a
wjehovah <wuyongx@gmail.com>
2017-07-03 14:59:42 +0800

    数据库密码，文件路径分割符

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -18,14 +18,14 @@ public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "123456";
+    private static final String JDBC_PASSWORD = "root";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
-    private static final String TEMPLATE_FILE_PATH = PROJECT_PATH + "\\src\\test\\resources\\generator\\template";//模板位置
+    private static final String TEMPLATE_FILE_PATH = PROJECT_PATH + "/src/test/resources/generator/template";//模板位置
 
-    private static final String JAVA_PATH = "\\src\\main\\java"; //java文件路径
-    private static final String RESOURCES_PATH = "\\src\\main\\resources";//资源文件路径
+    private static final String JAVA_PATH = "/src/main/java"; //java文件路径
+    private static final String RESOURCES_PATH = "/src/main/resources";//资源文件路径
 
     private static final String PACKAGE_PATH_SERVICE = packageConvertPath(SERVICE_PACKAGE);//生成的Service存放路径
     private static final String PACKAGE_PATH_SERVICE_IMPL = packageConvertPath(SERVICE_IMPL_PACKAGE);//生成的Service实现存放路径
@@ -35,7 +35,7 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
-        genCode("输入表名");
+        genCode("user");
     }
 
     public static void genCode(String... tableNames) {
@@ -212,6 +212,6 @@ public class CodeGenerator {
     }
 
     private static String packageConvertPath(String packageName) {
-        return String.format("\\%s\\", packageName.contains(".") ? packageName.replaceAll("\\.", "\\\\") : packageName);
+        return String.format("/%s/", packageName.contains(".") ? packageName.replaceAll("/.", "/") : packageName);
     }
 }
